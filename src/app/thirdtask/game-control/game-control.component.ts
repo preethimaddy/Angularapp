5664a68c5aa1c03f7b3958dcf239350bb64d496f
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2022-12-29 22:04:58 +0530

    gamecontrol,even odd component creation

---
+++ src/app/thirdtask/game-control/game-control.component.ts
@@ -0,0 +1,28 @@
+import { Component,OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.scss']
+})
+export class GameControlComponent {
+  @Output() intervalFired =new EventEmitter<number>();
+   interval: any;
+   lastNumber = 0;
+
+  constructor(){
+
+  }
+ngOnInit(){
+
+}
+  onStartGame(){
+   this.interval=setInterval(() => {
+    this.intervalFired.emit(this.lastNumber +1);
+    this.lastNumber++;
+   }, 1000); 
+  }
+  onPause(){
+
+  }
+}
